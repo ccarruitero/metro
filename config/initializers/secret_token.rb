27c3639dcc7f5a0236c1dbf3856422701e764f54
Cesar Carruitero <cesar@mozilla.pe>
2013-05-14 02:42:27 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metro::Application.config.secret_token = '0a47d7efd95ef6f7f61c126d31b5c9e93834c5d739f4301e1066e377b38c58d0b2bd07370ff14410fb7ecc97dd4f65da2175b2ab762487fcd750796d72ddf5b8'
