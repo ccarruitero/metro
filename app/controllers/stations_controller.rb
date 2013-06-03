4eb3ae15a5967520a85f9ff0cfa0ca76638c03ea
Cesar Carruitero <cesar@mozilla.pe>
2013-06-03 14:39:30 -0500

    fix public api

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,7 +2,7 @@ class StationsController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Station.all
+    respond_with Station.all, callback: params[:callback]
   end
 
   def show
